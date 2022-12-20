b92a8767b7a40f343b086a7cda6932f59d330d09
Egorov-Andrey <andreye1989@gmail.com>
2022-12-20 19:42:38 +0300

    task_2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
 ﻿
-Console.WriteLine("Введите число: ");
+// Task_1:
+
+// Console.WriteLine("Введите число: ");
+// string N = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(N[N.Length - 2]);
+
+// Task_2:
+
+// Console.WriteLine("Введите число: ");
+// string N = Console.ReadLine();
+
+// if (N.Length == 2)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//     Console.WriteLine(N[N.Length - 3]);
+// }
+
+
+
 
 
